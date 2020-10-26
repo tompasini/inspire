686a89a55fa847cb2082b7d2e8cc0e94779e18bf
tompasini <pasinitom91@gmail.com>
2020-10-26 09:00:46 -0600

    changed model file path to lowercase

--- app/Services/TodoService.js
+++ app/Services/TodoService.js
@@ -1,5 +1,5 @@
 import { ProxyState } from "../AppState.js";
-import Todo from "../Models/Todo.js";
+import Todo from "../models/Todo.js";
 import { api } from "../Services/AxiosService.js";
 
 let url = 'Tom/todos/'
